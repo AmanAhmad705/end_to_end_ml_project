109074cea93a563218d719be745e37fd5ac0c26f
AmanAhmad705 <ahmadaman90172@gmail.com>
2025-08-31 17:04:56 +0530

    fix: use cross-platform paths and correct preprocessor filename

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -6,47 +7,42 @@ from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
-        pass
+        # Use os.path.join for cross-platform paths
+        self.model_path = os.path.join("artifacts", "model.pkl")
+        self.preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\proprocessor.pkl'
             print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            model = load_object(file_path=self.model_path)
+            preprocessor = load_object(file_path=self.preprocessor_path)
             print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
 
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
 class CustomData:
-    def __init__(  self,
+    def __init__(
+        self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
         lunch: str,
         test_preparation_course: str,
         reading_score: int,
-        writing_score: int):
-
+        writing_score: int
+    ):
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
